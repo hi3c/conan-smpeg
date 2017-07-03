5b842cb1c307e2d98b504ee2b977de8160ccc04d
Tobias Hi3C <tobias+hi3c@hieta.se>
2017-07-03 14:17:50 +0200

    Inital commit

---
+++ conanfile.py
@@ -0,0 +1,40 @@
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+import os
+
+
+class SmpegConan(ConanFile):
+    name = "smpeg"
+    version = "2.0.0"
+    license = "<Put the package license here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    generators = "cmake"
+    requires = "SDL2/2.0.5@hi3c/experimental"
+
+    def source(self):
+        tools.download("https://www.libsdl.org/projects/smpeg/release/smpeg2-2.0.0.tar.gz", "smpeg.tar.gz")
+        tools.unzip("smpeg.tar.gz")
+        os.remove("smpeg.tar.gz")
+
+    def build(self):
+        atbe = AutoToolsBuildEnvironment(self)
+        atbe.flags.append("-Wno-narrowing")
+        atbe.fpic = True
+        atbe.configure(configure_dir="smpeg2-2.0.0")
+        atbe.make()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="smpeg2-2.0.0")
+        self.copy("*.lib", dst="lib", keep_path=False)
+
+        if self.options.shared:
+            self.copy("*.dll", dst="bin", keep_path=False)
+            self.copy("*.so*", dst="lib", keep_path=False)
+            self.copy("*.dylib", dst="lib", keep_path=False)
+        else:
+            self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["smpeg2"]
---
+++ test_package/example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <smpeg.h>
+
+int main() {
+  SMPEG_Info mp3info;
+
+  SMPEG_new(NULL, &mp3info, 0);
+}
